e564df637755cd32f342c628dee1813bc9c6ef5a
PrinceXXIV <91701011+princevvs@users.noreply.github.com>
2021-10-15 10:29:03 +0000

    Initial commit

---
+++ SnakeGame.py
@@ -0,0 +1,15 @@
+import turtle
+
+window = turtle.Screen()
+window.title("Retro Snake Game by @PrinceXXIV")
+window.bgcolor("e6b800")
+window.setup(width=600, height=600)
+window.tracer(0)
+
+
+
+
+
+
+
+window.mainloop()
