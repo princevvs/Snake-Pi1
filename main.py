a9841a84ec89f167adff6c30bc58e8e653f41f94
Prince XXIV <91701011+princevvs@users.noreply.github.com>
2021-10-18 14:03:06 +0000

    Fixed errors and continued to add to the game

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
+# 1/2/5
 import turtle
 import time
+import random
 
 delay = 0.1
 
-#These set of codes are used to set up the screen
+#1 These set of codes are used to set up the screen
 window = turtle.Screen()
 window.title("Retro Snake Game by @PrinceXXIV")
 window.bgcolor("Gold")
 window.setup(width=600, height=550)
 window.tracer(0) #This turns off any screen update
 
-#Snake head
+#2 Snake head
 snakehead = turtle.Turtle()
 snakehead.speed(0)
 snakehead.shape("circle")
@@ -19,7 +21,19 @@ snakehead.penup()
 snakehead.goto(0,0)
 snakehead.direction = "stop"
 
-#Functions
+#3 Snake Food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("square")
+food.color("green")
+food.penup()
+food.goto(0,100)
+
+# 4
+segments = []
+
+
+#2 Functions
 def move():
     if snakehead.direction == "up":
         y = snakehead.ycor()
@@ -58,12 +72,51 @@ window.onkeypress(go_left,"Left")
 window.onkeypress(go_right,"Right")
 
 #Main game Loop
+
+#5 Check for collision with the border
+if snakehead.xcor()>270 or snakehead.xcor()<-270 or snakehead.ycor()>270 or snakehead.ycor()<-270:
+    time.sleep(1)
+    snakehead.goto(0,0)
+    snakehead.direction = "stop"
+#5 Hide segments if collision occurs
+for segment in segments:
+    segment.goto(1000, 1000)
+
+#5 Clear the segment list
+segments.clear()
+
 while True:
     window.update()
 
+    #4 Check for collision with the food
+
+
+    if snakehead.distance(food) < 20:
+      #3 Move the food to a random spot on the screen
+      x = random.randint(-290,290)
+      y = random.randint(-290,290)
+      food.goto(x, y)
+
+    #4 add a segment
+      new_segment = turtle.Turtle()
+      new_segment.speed(0)
+      new_segment.shape("circle")
+      new_segment.color("brown")
+      new_segment.penup()
+      segments.append(new_segment)
+
+    #4 Move the end segments first in reverse order
+    for index in range(len(segments)-1, 0, -1):
+        x = segments[index-1].xcor()
+        y = segments[index-1].ycor()
+        segments[index].goto(x,y)
+    #4 Move segment 0 to where the head is
+    if len(segments) > 0:
+        x = snakehead.xcor()
+        y = snakehead.ycor()
+        segments[0].goto(x,y)
     move()
 
     time.sleep(delay)
-
 
 window.mainloop()
